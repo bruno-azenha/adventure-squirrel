ad22cd32e841d95c27a2990c71e02ba060ad290d
kelvin-wenjie-zhang <zhangwenjie8206@gmail.com>
2014-11-12 18:31:15 -0500

    add current_room attribute in player class

--- playersquirrel.py
+++ playersquirrel.py
@@ -2,12 +2,13 @@
 
 class Player(object):
 
-    def __init__(self, name):
+    def __init__(self):
 
         #self.name = name
         self.inventory = []
         self.score = 0
         self.moves = 0
+        self.current_room = None
         
     def __str__(self):
     	return 'Player: {0}\nScore: {1}\nMoves: {2}\nInventory: {3}\nAvailable Actions: {4}'.format(self.name, 
