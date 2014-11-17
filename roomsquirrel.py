222fc5bc07b2a4df4a8f2a6129975a9c602d80fa
kelvin-wenjie-zhang <zhangwenjie8206@gmail.com>
2014-11-16 23:34:32 -0500

    add removeItem()

--- roomsquirrel.py
+++ roomsquirrel.py
@@ -32,6 +32,11 @@ class RoomSquirrel():
     def AddItem(self, item):
         self.items.append(item)
 
+    # Method to remove an item to a room
+    # item is an index to the item in GAME.items  list
+    def RemoveItem(self, item):
+        self.items.remove(item)    
+
     #def remove_self(self):
         #for room in RoomSquirrel.instances:
         #    if self in room.dictOfConnections.values():
