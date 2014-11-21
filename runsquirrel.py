243be0798f85ae6b4347d092c0f2a44d9e0a6da0
kelvin-wenjie-zhang <zhangwenjie8206@gmail.com>
2014-11-21 00:45:25 -0500

    implement case 1 and case 2 action format

--- runsquirrel.py
+++ runsquirrel.py
@@ -90,9 +90,172 @@ def PlayGame(GAME, screen):
         screen = useful.PrintText(">> ", screen, 6, 0)   
         
         command = useful.GetInput(screen, 6, 3)
+        command = command.lower()
 
         if command == "quit":
             break
+
+        # clean up --> remove "the", "a", "an"
+        command_list = clean(command)
+
+        # case <verb>
+        if len(command_list) == 1: 
+            
+            # boolean type to indicate whether the action is successfully executed
+            result = handleActionFormat1(GAME, screen, command_list)
+            response(GAME, screen, command_list, result)
+
+        # case <verb> <item>
+        elif len(command_list) == 2:
+            
+            # boolean type to indicate whether the action is successfully executed
+            result = handleActionFormat2(GAME, screen, command_list)
+            response(GAME, screen, command_list, result)
+
+        # case <verb> <preposition> <item>
+        elif len(command_list) == 3:
+            # do something
+            print("NOT IMPLEMENTED")
+
+        # case <verb> <item> <preposition> <item>
+        elif len(command_list) == 4:
+            # do something
+            print("NOT IMPLEMENTED")
+        
+        # cannot find the action format
+        else:
+            screen = useful.PrintText("Sorry, I'm not quit sure what do you mean by \"" + command + "\"", screen, 8, 0)
+
+def clean(command):
+
+    command_list = command.split(" ")
+
+    while True:
+
+        #remove "the"
+        if "the" in command_list:
+            command_list.remove("the")
+
+        # remove "a"
+        elif "a" in command_list:
+            command_list.remove("a")
+
+        # remove "an"
+        elif "an" in command_list:
+            command_list.remove("an")
+
+        else:
+            break
+
+    return command_list
+
+def response(GAME, screen, command_list, result):
+
+    if result == False:
+        response = "Sorry, " + "\""
+        for i in command_list:
+            response += i
+        response += "\"" + " is not defined."
+
+        screen = useful.PrintText(response, screen, 10, 0)
+
+    elif result == True:
+        response = "OK then, what's next?"
+        screen = useful.PrintText(response, screen, 10, 0)
+
+# handle the case <verb>
+def handleActionFormat1(GAME, screen, command_list):
+
+    verb = command_list[0]
+
+    # if it's not a verb but it's inventory
+    inventory = ""
+    if verb == "inventory":
+        indecies = actionsquirrel.Inventory(GAME)
+        for i in indecies:
+            inventory += GAME.items[i].name + ", "
+        screen = useful.PrintText(inventory, screen, 8, 0)
+        return True
+
+    for def_act in actionsquirrel.DEFAULT_ACTIONS:
+
+        if verb == def_act:
+            screen = useful.PrintText("Where(What) do you want to " + verb + "?", screen, 8, 0)
+            return None
+            break
+
+    # else statement would not be executed if the for loop has been "break"
+    # --> we cannot find the verb in default actions
+    # --> let's search the customActions
+    else:
+        for action in GAME.customActions:
+            if action.verb == verb:
+                # then we execute the action
+                action.execute()
+                return True
+
+        # we cannot find the action
+        return False
+
+# handle the case <verb> <item>
+def handleActionFormat2(GAME, screen, command_list):
+
+    verb = command_list[0]
+    item = command_list[1]
+
+    for def_act in actionsquirrel.DEFAULT_ACTIONS:
+
+        if verb == def_act and verb == "pick":
+            for index in range(len(GAME.items)):
+                if GAME.items[index].name == item:
+                    if actionsquirrel.Pick(index, GAME) == True:
+                        return True
+                    else:
+                        screen = useful.PrintText(str(item)+" is not pickable", screen, 9, 0)
+                        return None
+            else:
+                screen = useful.PrintText(str(item)+ " does not exist", screen, 9, 0)
+                return None
+
+        elif verb == def_act and verb == "drop":
+            for index in range(len(GAME.items)):
+                if GAME.items[index].name == item:
+                    if actionsquirrel.Drop(index, GAME) == True:
+                        return True
+                    else:
+                        screen = useful.PrintText(str(item)+" is not dropable", screen, 9, 0)
+                        return None
+            else:
+                screen = useful.PrintText(str(item)+ " does not exist", screen, 9, 0)
+                return None
+
+        elif verb == def_act and verb == "examine":
+            for index in range(len(GAME.items)):
+                if GAME.items[index].name == item:
+                    description = actionsquirrel.Examine(index, GAME)
+                    screen = useful.PrintText(description, screen, 9, 0)
+                    return True
+
+            else:
+                screen = useful.PrintText(str(item)+ " does not exist", screen, 9, 0)
+                return None
+
+        # ---------other default actions-----------
+        # NOT YET IMPLEMENTED
+        # ---------other default actions-----------
+
+    # else statement would not be executed if the for loop has been "break"
+    # --> we cannot find the verb in default actions
+    # --> let's search the customActions
+    else:
+        for action in GAME.customActions:
+            if action.verb == verb:
+                # then we execute the action
+                action.execute()
+                return True
+
+        # we cannot find the action
+        return False
 
 
 # Wraps the curses changes to the terminal to prevent errors
